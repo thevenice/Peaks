6af0d4b9d8ccb59f572de6802b1fed3c8cc1654a
thevenice <pp886655@gmail.com>
2018-10-06 18:59:44 +0530

    Created  a JS file for 404 page.

---
+++ docs/user/PageNotFound.js
@@ -0,0 +1,7 @@
+let pageNotFound = document.createElement("div");
+let h1 = document.createElement("h1");
+let p = document.createElement("p");
+h1.innerHTML = "404";
+p.innerHTML = "Sorry, This Page Doesn't Exist";
+pageNotFound.appendChild(h1);
+pageNotFound.appendChild(p);
